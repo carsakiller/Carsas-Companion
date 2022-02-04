3229be6a2a65c470bae8386a71be4d2e59cee7d7
CrazyFluffyPony <crazyfluffypony@gmail.com>
2022-02-04 23:22:40 +0100

    fix wrong heartbeat logging

--- c2/c2.js
+++ c2/c2.js
@@ -101,7 +101,7 @@ module.exports = class C2 extends C2LoggingUtility {
 
 	handleGameMessage(message){
 		if(message.type === 'heartbeat'){
-			this.warn('♥')
+			this.log('♥')
 		} else {
 			this.log('handleGameMessage', message.type)
 		}
